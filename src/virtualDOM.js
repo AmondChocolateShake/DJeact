ffa922a918545f5806253aefe808ee3e7810ef1c
dongjour99 <dongjour99@gmail.com>
2025-10-30 21:04:41 +0900

    feature/3 declared funtions for virtual DOM.

---
+++ src/virtualDOM.js
@@ -0,0 +1,19 @@
+
+
+/**
+ * Create a Virtual DOM Object to compare Real DOM and create UI
+ * @param {} element 
+ */
+function createVirtualDOM(value) {
+  
+}
+
+
+/**
+ * Create a root Element(Search root id)
+ * @param {*} id
+ */
+function createRoot(id) {
+  
+}
+
